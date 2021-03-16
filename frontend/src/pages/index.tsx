269f94a770f36710054e669f6c2785b655b51d5d
haruyasu <harukun2002@gmail.com>
2021-03-16 21:44:08 +0900

    base

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { VFC } from "react";
+import { Layout } from "src/components/layout";
+
+const Home: VFC = () => {
+
+  return (
+    <Layout>
+        Click me!
+    </Layout>
+  );
+};
+
+export default Home;
